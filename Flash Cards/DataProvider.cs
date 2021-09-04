0937e9df0e81bdcd91dc0b7968e64428842a12f8
lexicw <42586346+lexicw@users.noreply.github.com>
2021-09-04 19:47:01 -0400

    Added a DataProvider class

---
+++ Flash Cards/DataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flash_Cards
+{
+    public abstract class DataProvider
+    {
+        List<string> allUsers = new List<string>();
+        List<string> allCardSets = new List<string>();
+        List<string> allCards = new List<string>();
+    }
+}
